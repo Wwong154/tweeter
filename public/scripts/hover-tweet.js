bedecbf9b2ce4d964444f5764f4696d1991b7271
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-12-15 21:59:31 +0000

    finished animation for now

--- public/scripts/hover-tweet.js
+++ public/scripts/hover-tweet.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $(".focus_effect").on('mouseover', function() {
-    $(".focus_effect").children('.shadow').css("background-color", "skyblue");
-    $(".focus_effect").children('.sideSpacer').children('.rightSpacer').css("background-color", "skyblue");
+    $(this).children('.shadow').css("visibility", "visible");
+    $(this).children('.sideSpacer').children('.rightSpacer').css("visibility", "visible");
+    $(this).children('.sideSpacer').children('article').children('.tweet-header').children('.handleName').css("visibility", "visible");
+    $(this).children('.sideSpacer').children('article').children('.tweet-content').css("font-weight", "600");
+    $(this).children('.sideSpacer').children('article').children('.tweet-header').children('.other-user').css("font-weight", "700");
   });
 
   $(".focus_effect").on('mouseout', function() {
-    $(".focus_effect").children('.shadow').css("background-color", "#f4f1ec");
-    $(".focus_effect").children('.sideSpacer').children('.rightSpacer').css("background-color", "#f4f1ec");
+    $(this).children('.shadow').css("visibility", "hidden");
+    $(this).children('.sideSpacer').children('.rightSpacer').css("visibility", "hidden");
+    $(this).children('.sideSpacer').children('article').children('.tweet-header').children('.handleName').css("visibility", "hidden");
+    $(this).children('.sideSpacer').children('article').children('.tweet-content').css("font-weight", "300");
+    $(this).children('.sideSpacer').children('article').children('.tweet-header').children('.other-user').css("font-weight", "400");
   });
 });
